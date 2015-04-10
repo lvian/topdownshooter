47376cf148bbdda95e339e74888b1dca26367aec
lvian <leandrovian@hotmail.com>
2015-04-10 16:49:26 -0300

    Lots of changes to main menu

---
+++ Assets/Scripts/Other/GUIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+	public UIPanel levelsPanel, upgradesPanel, creditsPanel;
+
+	public static GUIManager instance = null;
+
+	void Awake () {
+		if(instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy(gameObject);
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void disableTweenColor()
+	{
+		Debug.Log (UIEventTrigger.current.transform.GetChild(0).GetComponentInChildren<TweenColor>());
+		UIEventTrigger.current.transform.GetChild(0).GetComponentInChildren<TweenColor>().ResetToBeginning();
+		UIEventTrigger.current.transform.GetChild(0).GetComponentInChildren<TweenColor>().enabled = false;
+	}
+
+	public void ShowPanel(GameObject panel)
+	{
+		NGUITools.SetActive (panel, true);
+	}
+
+	public void DisablePanel(GameObject panel)
+	{
+		NGUITools.SetActive (panel, false);
+	}
+}
