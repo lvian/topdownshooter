b2021349768fe0f0ba9fad6a064f1bd247535c5d
Caco <caco.colombo@gmail.com>
2015-04-24 17:08:47 -0300

    Localization system prototype

---
+++ Assets/Scripts/Other/LabelLocalization.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabelLocalization : MonoBehaviour {
+	public string key;
+	private UILabel label;
+
+	// Use this for initialization
+	void Awake () {
+		label = GetComponent<UILabel>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		label.text = L18n.instance.GetText(key);
+	}
+}
