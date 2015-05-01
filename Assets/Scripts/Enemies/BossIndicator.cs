ad96e1bad780cea9bf4600d272f7a1bd68a59fc1
Caco <caco.colombo@gmail.com>
2015-05-01 18:05:51 -0300

    BossIndicator improvements

--- Assets/Scripts/Enemies/BossIndicator.cs
+++ Assets/Scripts/Enemies/BossIndicator.cs
@@ -6,11 +6,11 @@ public class BossIndicator : MonoBehaviour {
 	public GameObject player;
 	public Camera cCamera;
 	public Camera GUICamera;
-	private UISprite sprite;
+	private GameObject sprite;
 
 	// Use this for initialization
 	void Start () {
-		sprite = GetComponent<UISprite>();
+		sprite = transform.GetChild(0).gameObject;
 		player = GameObject.Find("PlayerBase");
 		cCamera = Camera.current;
 		GUICamera =(Camera) GameObject.FindGameObjectWithTag ("GUI Camera").GetComponent<Camera>();
@@ -23,10 +23,10 @@ public class BossIndicator : MonoBehaviour {
 			return;
 		if(boss != null && player != null){
 			if(boss.GetComponentInChildren<SpriteRenderer>().isVisible){
-				sprite.enabled = false;
+				NGUITools.SetActive (sprite, false);
 			}
 			else{
-				sprite.enabled = true;
+				NGUITools.SetActive (sprite, true);
 				if(cCamera == null){
 					cCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
 				}
@@ -40,7 +40,7 @@ public class BossIndicator : MonoBehaviour {
 					topRight.x - bottomLeft.x,
 					topRight.y - bottomLeft.y);
 
-				Vector3 bossPos = new Vector3(boss.transform.position.x, boss.transform.position.y, boss.transform.position.z);
+				Vector3 bossPos = new Vector3(boss.transform.position.x, boss.transform.position.y, 0f);
 
 				Vector3 newPos = new Vector3(
 					Mathf.Clamp(bossPos.x, cameraRect.xMin, cameraRect.xMax),
@@ -49,14 +49,42 @@ public class BossIndicator : MonoBehaviour {
 
 				Vector3 guiPos = cCamera.WorldToViewportPoint(newPos);
 
-
+				float sizeX = sprite.GetComponent<UISprite>().localSize.x;
+				float sizeY = sprite.GetComponent<UISprite>().localSize.y;
 				guiPos = GUICamera.ViewportToWorldPoint (guiPos);
 				transform.localPosition = transform.parent.InverseTransformPoint(guiPos);
+
+				newPos = transform.localPosition;
+				if(transform.localPosition.y >= (NGUITools.screenSize.y/2 - (sizeY/4))){
+					newPos.y -= (sizeY/2);
+				}
+				else if((int)(transform.localPosition.y) <= -(NGUITools.screenSize.y/2 - (sizeY/4))){
+					newPos.y += (sizeY/2);
+				}
+				Debug.Log(NGUITools.screenSize);
+				if(transform.localPosition.x >= (NGUITools.screenSize.x/2 - (sizeX/4))){
+					newPos.x -= (sizeX/2);
+				}
+				else if(transform.localPosition.x <= -(NGUITools.screenSize.x/2 - (sizeX/4))){
+					newPos.x += (sizeX/2);
+				}
+				//Debug.Log(newPos);
+				transform.localPosition = newPos;
 			}
 		}
 		else{
+			NGUITools.SetActive (sprite, false);
 			player = GameObject.Find("PlayerBase");
-			boss = GameObject.FindGameObjectsWithTag("Enemy")[0];
+			GameObject[] gos = GameObject.FindGameObjectsWithTag("Boss");
+			if(gos.Length > 0){
+				boss = gos[0];
+			}
+			UISprite bossIndicatorSprite;
+			bossIndicatorSprite = sprite.GetComponent<UISprite>();
+			bossIndicatorSprite.topAnchor.absolute = 16;
+			bossIndicatorSprite.bottomAnchor.absolute = 16;
+			bossIndicatorSprite.rightAnchor.absolute = 16;
+			bossIndicatorSprite.leftAnchor.absolute = 16;
 		}
 	}
 }
